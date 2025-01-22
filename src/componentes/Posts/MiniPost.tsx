4305927cdf19a4dbf1c45cbfc003103ff00dd6b3
Federico Crovetto <FedericoLCrovetto@gmail.com>
2025-01-22 12:00:51 -0300

    added class to tittle on minipost

--- src/componentes/Posts/MiniPost.tsx
+++ src/componentes/Posts/MiniPost.tsx
@@ -93,7 +93,7 @@ export class MiniPost extends React.Component<IPost, IMiniPost> {
 				<div ref={this.DivRef} className={this.state.InsideFormat}>
 					<header>
 						<span className="date">{this.state.fecha}</span>
-						<h2><a>{this.state.titulo}</a></h2>
+						<h2 className="titulo-post"><a>{this.state.titulo}</a></h2>
 						
 						{ 
 							this.state.selected &&						 
