eb6cbf9a5417e40966362bb2bf3dcd79f93bdebe
Federico Crovetto <FedericoLCrovetto@gmail.com>
2025-08-13 16:49:05 -0300

    updated api location

--- src/Env_Variables.ts
+++ src/Env_Variables.ts
@@ -1,4 +1,4 @@
 export class Env_Variables {
-    public static readonly Api_Url : string = "https://personalblog-api.glitch.me/"
+    public static readonly Api_Url : string = "http://localhost:3002/posts/"
     public static readonly Get_Posts : string = "getPosts"
 }
